726ce2074fb819e3a78dc4189b128e140f9eb97f
Facundo Rodriguez Eseverri <facundo.eseverri@gmail.com>
2018-10-17 23:45:20 -0300

    Base de datos actualizada

--- db/wikibeer.sql
+++ db/wikibeer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-10-2018 a las 22:23:45
+-- Tiempo de generación: 18-10-2018 a las 04:43:55
 -- Versión del servidor: 10.1.32-MariaDB
 -- Versión de PHP: 7.2.5
 
@@ -35,6 +35,21 @@ CREATE TABLE `cerveza` (
   `id_creador` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `cerveza`
+--
+
+INSERT INTO `cerveza` (`id_cerveza`, `nombre`, `precio`, `id_creador`) VALUES
+(8, 'Black IPA', 300, 8),
+(14, 'IPA', 200, 12),
+(15, 'Honey', 200, 13),
+(16, 'Barley wine', 100, 13),
+(17, 'Scotish', 400, 14),
+(18, 'IPA ', 100, 14),
+(19, 'Blonde', 500, 14),
+(20, 'Blonde', 100, 12),
+(21, 'Imperial Stout', 200, 15);
+
 -- --------------------------------------------------------
 
 --
@@ -53,7 +68,10 @@ CREATE TABLE `distribuidor` (
 
 INSERT INTO `distribuidor` (`id_creador`, `nombre`, `localidad`) VALUES
 (8, 'Antares', 'Mar del Plata'),
-(9, 'Eze', 'El cielo');
+(12, 'Gluck', 'Balcarce'),
+(13, 'Quarryman', 'Tandil'),
+(14, '713', 'Tandil'),
+(15, 'Patagonia', 'Bariloche');
 
 -- --------------------------------------------------------
 
@@ -66,6 +84,13 @@ CREATE TABLE `usuario` (
   `nombre` varchar(100) COLLATE latin1_spanish_ci NOT NULL,
   `clave` varchar(300) COLLATE latin1_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `clave`) VALUES
+(1, 'Faca', '$2y$12$OyKbUNv7MmFLEPMynJ3K5u7.m1PcAMfF3LMV7uf5.2JFZOml5cuk2');
 
 --
 -- Índices para tablas volcadas
@@ -98,19 +123,19 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `cerveza`
 --
 ALTER TABLE `cerveza`
-  MODIFY `id_cerveza` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cerveza` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de la tabla `distribuidor`
 --
 ALTER TABLE `distribuidor`
-  MODIFY `id_creador` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_creador` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
