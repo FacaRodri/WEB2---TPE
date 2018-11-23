a6e9d4505e846523efde432af990d930230a975d
Facundo Rodriguez Eseverri <facundo.eseverri@gmail.com>
2018-11-22 23:25:44 -0300

    Pagina funcional

--- templates/comentarios.tpl
+++ templates/comentarios.tpl
@@ -1,21 +1,24 @@
 <div class="container-fluid">
-
-    <input type="input" hidden="hidden" class="admin" value="{$smarty.session.admin}" data="admin">
-
-    <div class="form-group">
-        <textarea class="form-control" id="comentario" rows="5" placeholder="¡Comentanos aqui!"></textarea>
-        <label class="editarLabel" for="puntaje">PUNTAJE</label>
-        <select class="form-control" id="puntaje">
+        <input type="input" hidden="hidden" class="admin" value="{$smarty.session.admin}" data="admin">
+{if (isset($smarty.session.admin))}
+        <div class="form-group">
+            <textarea class="form-control" id="comentario" rows="5" placeholder="¡Comentanos aqui!"></textarea>
+            <label class="editarLabel" for="puntaje">PUNTAJE</label>
+         <select class="form-control" id="puntaje">
             <option>1</option>
             <option>2</option>
             <option>3</option>
             <option>4</option>
             <option>5</option> 
-        </select>
-        <input type="input" hidden="hidden" id="id_usuario" value="" data="{$smarty.session.id_usuario}">
-        <input type="input" hidden="hidden" id="id_cerveza" value="">
-    </div>
+         </select>
+         <input type="input" hidden="hidden" id="id_usuario" value="" data="{$smarty.session.id_usuario}">
+            <input type="input" hidden="hidden" id="id_cerveza" value="">
+        </div>
     
   <button type="submit" class="btn btn-primary" id="agregarComentario" name="agregarComentario">ENVIAR</button>
-
+    {else}
+    <div>
+    </div>
+{/if}
+    
 </div>
--- templates_c/0ce0c3d0b8b93062576560fc04c8db53a53bd7c3_0.file.comentarios.tpl.php
+++ templates_c/0ce0c3d0b8b93062576560fc04c8db53a53bd7c3_0.file.comentarios.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2018-11-23 02:34:32
+/* Smarty version 3.1.33, created on 2018-11-23 03:19:25
   from 'C:\xampp\htdocs\WikiBeerTPE\templates\comentarios.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5bf75928198d22_40686996',
+  'unifunc' => 'content_5bf763ad5831c6_41510315',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '0ce0c3d0b8b93062576560fc04c8db53a53bd7c3' => 
     array (
       0 => 'C:\\xampp\\htdocs\\WikiBeerTPE\\templates\\comentarios.tpl',
-      1 => 1542936871,
+      1 => 1542939564,
       2 => 'file',
     ),
   ),
@@ -20,28 +20,31 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5bf75928198d22_40686996 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5bf763ad5831c6_41510315 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div class="container-fluid">
-
-    <input type="input" hidden="hidden" class="admin" value="<?php echo $_SESSION['admin'];?>
+        <input type="input" hidden="hidden" class="admin" value="<?php echo $_SESSION['admin'];?>
 " data="admin">
-
-    <div class="form-group">
-        <textarea class="form-control" id="comentario" rows="5" placeholder="¡Comentanos aqui!"></textarea>
-        <label class="editarLabel" for="puntaje">PUNTAJE</label>
-        <select class="form-control" id="puntaje">
+<?php if ((isset($_SESSION['admin']))) {?>
+        <div class="form-group">
+            <textarea class="form-control" id="comentario" rows="5" placeholder="¡Comentanos aqui!"></textarea>
+            <label class="editarLabel" for="puntaje">PUNTAJE</label>
+         <select class="form-control" id="puntaje">
             <option>1</option>
             <option>2</option>
             <option>3</option>
             <option>4</option>
             <option>5</option> 
-        </select>
-        <input type="input" hidden="hidden" id="id_usuario" value="" data="<?php echo $_SESSION['id_usuario'];?>
+         </select>
+         <input type="input" hidden="hidden" id="id_usuario" value="" data="<?php echo $_SESSION['id_usuario'];?>
 ">
-        <input type="input" hidden="hidden" id="id_cerveza" value="">
-    </div>
+            <input type="input" hidden="hidden" id="id_cerveza" value="">
+        </div>
     
   <button type="submit" class="btn btn-primary" id="agregarComentario" name="agregarComentario">ENVIAR</button>
-
+    <?php } else { ?>
+    <div>
+    </div>
+<?php }?>
+    
 </div><?php }
 }
